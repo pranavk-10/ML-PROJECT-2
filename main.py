cf061ad7323b91c26ce57477454365313f873b3a
Pranav K <mr.pranavkamble10@gmail.com>
2025-06-18 17:33:43 +0530

    MODEL TRAINER

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig, DataTransformationConfig
 from networksecurity.components.data_transformation import DataTransformation
 
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import ModelTrainerConfig
+
 import sys
 
 if __name__ == "__main__":
@@ -36,6 +39,12 @@ if __name__ == "__main__":
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         logging.info("Data Transformation is completed")
         print(data_transformation_artifact)
+
+        logging.info("Model Training started")
+        model_trainer_config =  ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer = ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+        logging.info("Model Training is completed")
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
